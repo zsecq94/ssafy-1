d0fb0e46a07ea0c5897dad100e11cee8e546d754
배중권 <zsecq2@naver.com>
2022-09-06 18:40:25 +0900

    0906_1

--- 3. django/django/05_django/articles/forms.py
+++ 3. django/django/05_django/articles/forms.py
@@ -2,28 +2,12 @@ from django import forms
 from .models import Article
 
 
-# class ArticleForm(forms.Form):
-#     NATION_A = 'kr'
-#     NATION_B = 'ch'
-#     NATION_C = 'jp'
-#     NATIONS_CHOICES = [
-#         (NATION_A, '한국'),
-#         (NATION_B, '중국'),
-#         (NATION_C, '일본'),
-#     ]
-
-#     title = forms.CharField(max_length=10)
-#     content = forms.CharField(widget=forms.Textarea)
-#     nation = forms.ChoiceField(choices=NATIONS_CHOICES)
-#     nation = forms.ChoiceField(choices=NATIONS_CHOICES, widget=forms.RadioSelect)
-
-
 class ArticleForm(forms.ModelForm):
     title = forms.CharField(
         label='제목',
         widget=forms.TextInput(
             attrs={
-                'class': 'my-title form-control',
+                'class': 'my-title',
                 'placeholder': 'Enter the title',
                 'maxlength': 10,
             }
@@ -34,7 +18,7 @@ class ArticleForm(forms.ModelForm):
         label='내용',
         widget=forms.Textarea(
             attrs={
-                'class': 'my-content form-control',
+                'class': 'my-content',
                 'placeholder': 'Enter the content',
                 'rows': 5,
                 'cols': 50,
