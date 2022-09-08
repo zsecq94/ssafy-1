8c27647d0e6c3ea664ece615561aa103c52aaaa8
baejoongkwon <zsecq2@naver.com>
2022-09-08 15:44:16 +0900

    0908_2

---
+++ 3. django/django_실습/day7/articles/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods, require_safe, require_POST
+from django.contrib.auth.decorators import login_required
+
+from articles.forms import ArticleForm
+from articles.models import Article
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@require_safe
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
+    
+@login_required
+@require_POST
+def delete(request, pk):
+    if request.user.is_authenticated:
+        article = Article.objects.get(pk=pk)
+        article.delete()
+    return redirect('articles:index')
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
