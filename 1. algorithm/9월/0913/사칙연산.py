988b638fcfe5ffeefdf4b99e22a4115b58b64bc2
zsecq94 <zsecq94@gmail.com>
2022-09-15 18:01:28 +0900

    0915_2

--- 1. algorithm/9월/0913/사칙연산.py
+++ 1. algorithm/9월/0913/사칙연산.py
@@ -1,29 +1,39 @@
-import sys; sys.stdin = open('123.txt', 'r')
+import sys
+sys.stdin = open('123.txt', 'r')
 
-def ino(root):
-    if root <= N:
-        ino(root*2)
-        ino(root*2+1)
-        if tree[root] == '-':
-            tree[root] = tree[root*2] - tree[root*2+1]
-        elif tree[root] == '+':
-            tree[root] = tree[root*2] + tree[root*2+1]
-        elif tree[root] == '*':
-            tree[root] = tree[root*2] * tree[root*2+1]
-        elif tree[root] == '/':
-            tree[root] = tree[root*2] // tree[root*2+1]
+def pos(root):
+    if root <= N+1:
+        pos(tree[root][2])
+        pos(tree[root][3])
+        if tree[root][1] == '-':
+            tree[root][1] = tree[root][2] - tree[root][3]
+        # elif tree[root][1] == '+':
+        #     tree[root] = tree[root][0] + tree[root][1]
+        # elif tree[root][1] == '*':
+        #     tree[root] = tree[root][0] * tree[root][1]
+        # elif tree[root][1] == '/':
+        #     tree[root] = tree[root][0] // tree[root][1]
 
-T = 10
+T = 5
 for tc in range(1, T+1):
     N = int(input())
-    tree = [0] * (N+1)
+    tree = [[] for _ in range(N+1)]
 
-    for i in range(1, N+1):
+    for i in range(N):
         arr = input().split()
-        if arr[1].isdigit():
-            tree[i] = int(arr[1])
-        else:
-            tree[i] = arr[1]
+        v = int(arr[0])
 
-    ino(1)
-    print(tree[1])
+        for j in arr:
+            if j.isdigit():
+                tree[v].append(int(j))
+            else:
+                tree[v].append(j)
+    print(tree)
+    # pos(1)
+
+
+
+    # print(tree)
+
+    # pos(1)
+    # print(tree)
---
+++ 1. algorithm/9월/0915/12423.py
@@ -0,0 +1,4 @@
+if type(123) == int:
+    print(1)
+else:
+    print(2)
