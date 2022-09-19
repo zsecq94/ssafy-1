3e6283eba47119e92f395920fcffdc8b6a4b2a71
zsecq94 <zsecq94@gmail.com>
2022-09-20 01:27:35 +0900

    220920 1:27

--- 1. algorithm/스터디/스타트링크.py
+++ 1. algorithm/스터디/스타트링크.py
@@ -1,19 +1,21 @@
 import sys; sys.stdin = open('123.txt', 'r')
 
-# f = 전체 층 수, s = 지금 있는 층, g = 도착 층
-T = int(input())
-for tc in range(1, T+1):
-    f, s, g, u, d = map(int, input().split())
+def bfs(f, s, g, u, d):
+    st = [s]
+    while st:
+        go = st.pop(0)
+        if go == g:
+            return visit[go]
 
-    cnt = 0
-    while s != g:
-        if s < g:
-            s += u
-            cnt += 1
-        elif d == 0:
-            cnt = 'use the stairs'
-            break
-        else:
-            s -= d
-            cnt += 1
-    print(cnt)
+        for ngo in (go + u, go - d):
+            if 0 < ngo <= f and visit[ngo] == 0:
+                st.append(ngo)
+                visit[ngo] = visit[go] + 1
+
+    return 'use the stairs'
+
+# f = 전체 층 수, s = 지금 있는 층, g = 도착 층
+f, s, g, u, d = map(int, input().split())
+
+visit = [0] * (f+1)
+print(bfs(f, s, g, u, d))
