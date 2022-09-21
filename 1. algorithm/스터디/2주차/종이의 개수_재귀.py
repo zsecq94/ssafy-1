a70a3ed1cb53e75e6ce5ea0fe847aa9a57c1b8aa
zsecq94 <zsecq94@gmail.com>
2022-09-22 00:48:35 +0900

    220922 00:48

--- 1. algorithm/스터디/2주차/종이의 개수_재귀.py
+++ 1. algorithm/스터디/2주차/종이의 개수_재귀.py
@@ -1,12 +1,32 @@
 import sys; sys.stdin = open('123.txt', 'r')
 
-def f(q):
-    if q == N+1:
-        return
+def f(x, y, a):
+    global cntA, cntB, cntC
+    check = arr[x][y]
+    for i in range(x, x+a):
+        for j in range(y, y+a):
+            if check != arr[i][j]:
+                for n in range(3):
+                    for m in range(3):
+                        f(x+n*a//3, y+m*a//3, a//3)
+                return
+    # check와 다른게 나올때 마다 추가,
+    if check == -1:
+        cntA += 1
+    elif check == 0:
+        cntB += 1
     else:
+        cntC += 1
 
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+cntA = 0
+cntB = 0
+cntC = 0
+# 위치 계산을 위해 x, y, N을 보냄
+f(0, 0, N)
 
-f(0)
+print(cntA)
+print(cntB)
+print(cntC)
