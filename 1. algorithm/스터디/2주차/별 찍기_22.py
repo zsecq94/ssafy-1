08a68cee0424dceb31a2906384ca73be2bc8c505
배중권 <zsecq2@naver.com>
2022-09-23 01:31:06 +0900

    220923 01:30

--- 1. algorithm/스터디/2주차/별 찍기_22.py
+++ 1. algorithm/스터디/2주차/별 찍기_22.py
@@ -1,26 +1,39 @@
-import sys; sys.stdin = open('123.txt', 'r')
+# import sys; sys.stdin = open('123.txt', 'r')
 
 def f(v, x, y, q):
     if v == 1:
-        arr[b//2][b//2] = '*'
-
+        arr[a//2][b//2] = '*'
+        arr[a//2+1][b//2] = '*'
+        return
+    # y = i, q = j
     else:
         for i in range(x, y):
             for j in range(x, q):
-                if i == x or j == x or i == y-1 or (j == q-1 and i >= x+2) or (i == x+2 and j > q-4):
-                    arr[i][j] = '*'
+                if i < a and j < b:
+                    if i == x or j == x or i == y-1:
+                        arr[i][j] = '*'
+                    if (j == q-1 and i >= x+2) or (i == x+2 and j > q-4):
+                        arr[i][j] = '*'
         f(v-1, x+2, y-2, q-2)
+
 
 N = int(input())
 a = 3 + ((N-1)*4)
 b = 1 + ((N-1)*4)
-if N == 1:
-    arr = [[' ']]
-else:
-    arr = [[' '] * b for _ in range(a)]
-f(N, 0, a, b)
+arr = [[' '] * b for _ in range(a)]
 
-for i in range(b):
-    for j in range(a):
-        print(arr[i][j], end='')
-    print()
+if N == 1:
+    print('*')
+else:
+    f(N, 0, a, b)
+    if N == 1:
+        print('*')
+    else:
+        for i in range(a):
+            for j in range(b):
+                if i == 1:
+                    print('*')
+                    break
+                print(arr[i][j], end='')
+            if i != 1:
+                print()
