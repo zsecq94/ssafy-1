46baa0c0537e2cd37af363b06adeb5218c5f8d8a
zsecq94 <zsecq94@gmail.com>
2022-10-14 02:24:09 +0900

    221014 02:24

--- 1. algorithm/스터디/5주차/치즈.py
+++ 1. algorithm/스터디/5주차/치즈.py
@@ -1,19 +1,36 @@
 import sys; sys.stdin = open('123.txt', 'r')
-
-def melt(x, y):
-    a = 1
-    for xx, yy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        nx = x + xx
-        ny = y + yy
-        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == a-1:
-            arr[x][y] = a-1
-            a += 1
-            return
+from collections import deque
+def melt(i, j):
+    visited = [[False] * m for _ in range(n)]
+    visited[i][j] = True
+    q = deque()
+    q.append((i, j))
+    while q:
+        x, y = q.popleft()
+        for xx, yy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nx = x + xx
+            ny = y + yy
+            if 0<=nx<n and 0<=ny<m and visited[nx][ny] == False:
+                if arr[nx][ny] == 1:
+                    arr[nx][ny] = 0
+                    visited[nx][ny] = True
+                else:
+                    q.append((nx,ny))
+                    visited[nx][ny] = True
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
+cheeze_lst = [0]
+while True:
+    cheeze_cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1:
+                cheeze_cnt += 1
+    if cheeze_cnt == 0:
+        break
+    cheeze_lst.append(cheeze_cnt)
+    melt(0, 0)
 
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 1:
-            melt(i, j)
+print(len(cheeze_lst)-1)
+print(cheeze_lst[-1])
