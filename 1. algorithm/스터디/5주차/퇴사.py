2d33f9b07b697b1f64a6e1d6eab1617f108c4fde
zsecq94 <zsecq94@gmail.com>
2022-10-18 02:03:54 +0900

    221018 02:03

--- 1. algorithm/스터디/5주차/퇴사.py
+++ 1. algorithm/스터디/5주차/퇴사.py
@@ -1,2 +1,21 @@
-import sys; sys.stdin = open('123.txt', 'r')
+# import sys; sys.stdin = open('123.txt', 'r')
 
+N = int(input())
+day = []
+money = []
+dp = [0] * (N + 1)
+maxV = 0
+
+for i in range(N):
+    a, b = map(int, input().split())
+    day.append(a)
+    money.append(b)
+
+for i in range(N-1, -1, -1):
+    time = day[i] + i
+    if time <= N:
+        dp[i] = max(money[i] + dp[time], maxV)
+        maxV = dp[i]
+    else:
+        dp[i] = maxV
+print(maxV)
