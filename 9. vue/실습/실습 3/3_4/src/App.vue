6506d55eb31b968815c099446f790a0449dcf3f1
zsecq94 <zsecq94@gmail.com>
2022-11-02 17:59:55 +0900

    221102 17:59

---
+++ 9. vue/실습/실습 3/3_4/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>소득세 계산기</h1>
+    <Income/>
+  </div>
+</template>
+
+<script>
+import Income from '@/components/Income'
+
+export default {
+  name: 'App',
+  components: {
+    Income,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 9. vue/실습/실습 3/3_4/src/components/Income.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>연봉 입력 (만원): 
+      <input 
+        type="number"
+        v-model.number="incomeMoney"
+      ></p>
+    <p>세액감면액 (만원):
+      <input 
+        type="number"
+        v-model.number="asdMoney"
+      ></p>
+    <Taxrate :income-props="incomeMoney"
+    :abs-props="asdMoney"/>
+    <hr>
+    <Taxrate/>
+  </div>
+</template>
+
+<script>
+import Taxrate from '@/components/Taxrate'
+
+export default {
+  name: 'Income',
+  components: {
+    Taxrate,
+  },
+  data: function () {
+    return {
+      incomeMoney: null,
+      asdMoney: null,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
