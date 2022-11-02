5f85378b72e682eca1c6af806f9ff3a74afa3796
baejoongkwon <zsecq2@naver.com>
2022-11-02 17:59:55 +0900

    221102 17:59

---
+++ 9. vue/실습/실습 3/3_4/src/components/Taxrate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>종합소득금액: {{ incomeProps }} 만원</h2>
+    <h2>종합소득공제:  만원</h2>
+    <h2>과세표준:  만원</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Taxrate',
+  props: {
+    incomeProps: Number,
+    absProps: Number,
+  },
+}
+</script>
+
+<style>
+
+</style>
