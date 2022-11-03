abde75eeee1778a763a4960327ab218945792f20
zsecq94 <zsecq94@gmail.com>
2022-11-03 17:12:17 +0900

    221103 17:12

---
+++ 9. vue/실습/실습 4/4_3/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div id="div1">
+      <Timetable @create-time="createTime"
+      @create-name="createName"/>
+    </div>
+    <div id="div2">
+      <Mylist :time-create="times" :name-create="names"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Timetable from '@/components/Timetable'
+import Mylist from '@/components/Mylist'
+
+
+export default {
+  name: 'App',
+  components: {
+    Timetable,
+    Mylist,
+  },
+  data: function () {
+    return {
+      times: [],
+      names: [],
+    }
+  },
+  methods: {
+    createTime: function (time) {
+      this.times.push(time)
+    },
+    createName: function (name) {
+      this.names.push(name)
+    }
+  }
+
+}
+</script>
+
+<style>
+#div1 {
+  width: 450px;
+  float: left;
+}
+#div2 {
+  width: 450px;
+  float: left;
+  background-color: #658DC63D;
+}
+#app {
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  margin-top: 60px;
+  color: #424242;
+}
+</style>
---
+++ 9. vue/실습/실습 4/4_3/src/components/Mylist.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>상담 신청 현황</h2>
+    <h3>상담 선생님</h3>
+    <p>성함 : <span v-for="(name, index) in nameCreate[0]" :key="index">{{name}} </span></p>
+    <hr id="hr2">
+    <h3>예약 현황</h3>
+    <p>시간들 : <span v-for="(time, index) in timeCreate[0]" :key="index">{{time}} </span></p>
+    <hr id="hr2">
+    <img id="img1" src="../assets/ssafy-logo.png" alt="asd">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mylist',
+  props: {
+    timeCreate: null,
+    nameCreate: null,
+  },
+  methods: {
+    clear: function () {
+      console.log('clear1')
+      if (this.timeCreate.length > 0 ||this.nameCreate.length > 0) {
+        this.timeCreate = null
+        this.nameCreate = null
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#hr2 {
+  margin: 20px;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+#img1 {
+  margin-bottom: 50px;
+}
+</style>
