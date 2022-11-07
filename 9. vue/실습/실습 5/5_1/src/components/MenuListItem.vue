e438309d1786462f435e989e973f4a387516c268
baejoongkwon <zsecq2@naver.com>
2022-11-07 18:00:19 +0900

    221106 18:00

---
+++ 9. vue/실습/실습 5/5_1/src/components/MenuListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h4 @click="selectMenu">{{ menu.title }}</h4>
+    <p>{{ menu.price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuListItem',
+  props: {
+    menu: Object,
+  },
+  methods: {
+    selectMenu() {
+      // console.log(1234)
+      this.$store.commit('updateMenuList', this.menu)
+    },
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 9. vue/실습/실습 5/5_1/src/components/SizeList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>2. 사이즈를 고르세요.</h1>
+    <SizeListItem
+      v-for="size in sizeList" :key="size.name"
+      :size="size"
+    />
+  </div>
+</template>
+
+<script>
+import SizeListItem from '@/components/SizeListItem'
+
+export default {
+  name: 'SizeList',
+  components: {
+    SizeListItem,
+  },
+  methods: {
+    onSelectMenu: function () {},
+  },
+  computed: {
+    sizeList: function () {
+      return this.$store.state.sizeList
+    },
+  },
+}
+</script>
+
+<style>
+</style>
