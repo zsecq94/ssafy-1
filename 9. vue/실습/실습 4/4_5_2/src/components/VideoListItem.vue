0ab2261e11f5b22ab453eae9558ff7e930ff505b
baejoongkwon <zsecq2@naver.com>
2022-11-10 17:59:45 +0900

    go

---
+++ 9. vue/실습/실습 4/4_5_2/src/components/VideoListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="div1" @click="goApp">
+    <img :src="thumbnails" alt="">
+    {{ video.snippet.title }}
+    <!-- {{video}} -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: {
+    video: Object,
+    thumbnails: String,
+  },
+  methods: {
+    goApp() {
+      this.$emit('goApp', this.video)
+    }
+  }
+}
+</script>
+
+<style>
+#div1 {
+  display: flex;
+}
+</style>
