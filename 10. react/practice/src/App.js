472ffba6f96f1e0064c58cc6c0102a25eb966c5e
zsecq94 <zsecq94@gmail.com>
2023-01-17 23:31:42 +0900

    230117 23:31

--- 10. react/practice/src/App.js
+++ 10. react/practice/src/App.js
@@ -1,89 +1,15 @@
-import * as React from 'react';
+import "./App.css";
 
-import CreateVote from './pages/CreateVote'
-import MyVote from './pages/MyVote'
-import Vote from './pages/Vote'
-
-// MUI
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
-const drawerWidth = 240;
-
-function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+function App() {
+  const name = "리액트";
   return (
-    <Box sx={{ display: 'flex' }}>
-      {/* <CssBaseline /> */}
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            LOGO
-          </Typography>
-          <Button color="inherit">Vote</Button>
-          <Button color="inherit">MyVote</Button>
-          <Button color="inherit">CreateVote</Button>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box component="main" sx={{ p: 3 }}>
-      </Box>
-    </Box>
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
   );
 }
 
-export default DrawerAppBar;
+export default App;
