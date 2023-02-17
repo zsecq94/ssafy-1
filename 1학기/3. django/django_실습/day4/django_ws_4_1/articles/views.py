878b95569763d66023310c9529b2e8c1287a65dc
zsecq94 <zsecq94@gmail.com>
2023-02-17 15:10:04 +0900

    파일구조 수정

---
+++ 1학기/3. django/django_실습/day4/django_ws_4_1/articles/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import redirect, render
+
+import articles
+from .models import Article
+
+# Create your views here.
+def index(request):
+    article = Article.objects.all()
+    context = {
+        'articles': article,
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article(title=title, content=content)
+    article.save()
+
+    return redirect('articles:index')
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
